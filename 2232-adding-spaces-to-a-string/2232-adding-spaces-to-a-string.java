0a06b4d113a9edbf7e013edaa995236618a098b7
Madhushree Ramachandran <137623310+MadhushreeRamachandran@users.noreply.github.com>
2024-12-03 12:26:32 -0500

    Time: 26 ms (40.66%), Space: 79.9 MB (70.45%) - LeetHub

---
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static String addSpaces(String s, int[] spaces) {
+        StringBuilder result = new StringBuilder();
+        int spaceIndex = 0;
+        int n = spaces.length;
+
+        for (int i = 0; i < s.length(); i++) {
+            // Check if the current index matches a space index
+            if (spaceIndex < n && i == spaces[spaceIndex]) {
+                result.append(" "); // Add a space
+                spaceIndex++;       // Move to the next space index
+            }
+            result.append(s.charAt(i)); // Add the current character
+        }
+
+        return result.toString();
+        
+    }
+}
