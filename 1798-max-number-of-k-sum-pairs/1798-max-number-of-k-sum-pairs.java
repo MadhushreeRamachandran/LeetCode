19fdca6341d5b2d4d605af00e2815b15d36381b5
Madhushree Ramachandran <137623310+MadhushreeRamachandran@users.noreply.github.com>
2024-12-31 17:54:35 +0530

    Time: 37 ms (29.22%), Space: 55.4 MB (91.36%) - LeetHub

---
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        int operations = 0;
+
+        for (int num : nums) {
+            int complement = k - num;
+            if (freqMap.getOrDefault(complement, 0) > 0) {
+                operations++;
+                freqMap.put(complement, freqMap.get(complement) - 1);
+            } else {
+                freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+            }
+        }
+
+        return operations;
+    }
+}
